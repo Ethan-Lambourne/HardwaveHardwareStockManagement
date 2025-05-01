ca346e9766f7c698f638bef69e3564f640a12934
Ethan Lambourne <100215369+Ethan-Lambourne@users.noreply.github.com>
2025-05-01 03:00:22 +0100

    Update DbInitializer.cs to include initial admin account for setup

--- DataManagement/DbInitializer.cs
+++ DataManagement/DbInitializer.cs
@@ -6,8 +6,15 @@ namespace HardwaveStockManagement.DataManagement
         public static void Initialize(ItemContext itemContext, UserContext userContext, OrderContext orderContext) 
         {
             itemContext.Database.EnsureCreated();
-            userContext.Database.EnsureCreated();
+            var wasUserDatabaseCreated = userContext.Database.EnsureCreated();
             orderContext.Database.EnsureCreated();
+        
+            if (wasUserDatabaseCreated) 
+            {
+                UserRepository _userRepository = new(userContext);
+                User initialAdminUser = new(Guid.NewGuid(), "Admin1", MD5.HashData(ASCIIEncoding.ASCII.GetBytes("admin")), true);
+                _userRepository.AddUser(initialAdminUser);
+            }
         }
     }
 }
